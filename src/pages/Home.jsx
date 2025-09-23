feb3d0aea7259ac5038c9abbf158662572379729
Mayor-chuke34 <chukwukao083@gmail.com>
2025-09-23 01:17:46 +0100

    updated my components and pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Hero />
+      <FeaturedSection />
+      <Banner />
+      <Testimonial />
+      <NewsLetter />
+    </>
   )
 }
 
