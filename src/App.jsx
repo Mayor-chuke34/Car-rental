c3722eb53aaaae81613efb323f7a1c5e52099f61
Mayor-chuke34 <chukwukao083@gmail.com>
2025-09-24 13:26:51 +0100

    updated my components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
 import Footer from './components/Footer'
+import Layout from './pages/Owner/Layout'
+import Dashboard from './pages/Owner/Dashboard'
+import AddCar from './pages/Owner/AddCar'
+import ManageCars from './pages/Owner/ManageCars'
+import ManageBookings from './pages/Owner/ManageBookings'
 
 const App = () => {
 
@@ -21,6 +26,12 @@ const isOwnerPath = useLocation().pathname.startsWith('/owner')
         <Route path='/car-details/:id'  element={<CarDetails/>}/>
         <Route path='/cars'  element={<Cars/>}/>
         <Route path='/my-bookings'  element={<MyBookings/>}/>
+        <Route path= '/owner' element={<Layout />}>
+          <Route  index element={<Dashboard />}/>
+          <Route path="add-car" element={<AddCar />}/>
+          <Route  path="manage-cars" element={<ManageCars />}/>
+          <Route  path="manage-bookings" element={<ManageBookings />}/>
+        </Route>
       </Routes> 
         {!isOwnerPath && <Footer />}
       
--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -5,6 +5,7 @@ import Title from '../components/Title'
 const MyBookings = () => {
 
     const [bookings, setBookings] = useState([])
+    const currency = import.meta.env.VITE_CURRENCY
 
     const fetchMyBookings = async ()=>{
         setBookings(dummyMyBookingsData)
@@ -49,7 +50,24 @@ const MyBookings = () => {
                                     <p>{booking.pickupDate.split('T')[0]} to {booking.returnDate.split('T')[0]}</p>
                                 </div>
                             </div>
+
+                            <div className='flex items-start gap-2 mt-3'>
+                                <img src={assets.location_icon_colored} alt=""  className='w-4 h-4 mt-1'/>
+                                <div>
+                                    <p className='text-gray-500'>Pick-up Location</p>
+                                    <p>{booking.car.location}</p>
+                                </div>
+                            </div>
                         </div>
+
+                    {/* Price */}
+                    <div className='md:col-span-1 flex flex-col justify-between gap-6'>
+                        <div className='text-sm text-gray-500 text-right'>
+                            <p>Total Price</p>
+                            <h1 className='text-2xl font-semibold text-primary'>{currency}{booking.price}</h1>
+                            <p>Booked on {booking.createdAt.split('T')[0]}</p>
+                        </div>
+                    </div>
 
             </div>
         ))}
