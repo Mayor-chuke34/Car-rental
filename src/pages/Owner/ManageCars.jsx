bcebe6d78e07d25f5d3a0394e92f8f4ce7377e22
Mayor-chuke34 <chukwukao083@gmail.com>
2025-09-27 15:03:28 +0100

    updated pages

--- src/pages/Owner/ManageCars.jsx
+++ src/pages/Owner/ManageCars.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyCarData } from '../../assets/assets'
+import Title from '../../components/owner/Title'
 
 const ManageCars = () => {
+
+    const [cars, setCars] = useState([])
+
+    const fetchOwnerCars = async ()=>{
+        setCars(dummyCarData)
+    }
+
+    useEffect(()=>{
+        fetchOwnerCars()
+    },[])
+        
   return (
-    <div>
+    <div className='px-4 pt-10 md:px-10 w-full'>
       
+    <Title title="Manage Cars" subTitle="View all listed cars, update their details, or remove them from the booking platform."/>
+
     </div>
   )
 }
