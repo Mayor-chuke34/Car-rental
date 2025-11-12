cc1bafc28e8210804185c32a014ff3e8f82dd207
Mayor-chuke34 <chukwukao083@gmail.com>
2025-11-12 16:00:43 +0100

    updated components and pages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useAppContext } from '../context/AppContext';
 
-const Login = ({setShowLogin}) => {
+const Login = () => {
+
+    const {setShowLogin, axios, setToken, navigate} = useAppContext()
 
     const [state, setState] = React.useState("login");
     const [name, setName] = React.useState("");
@@ -8,11 +11,28 @@ const Login = ({setShowLogin}) => {
     const [password, setPassword] = React.useState("");
 
     const onSubmitHandler = async (event)=>{
-      event.preventDefault();
+        try {
+            event.preventDefault();
+            const {data} = await axios.post(`/api/user/${state}`, {name, email, password})
+
+            if (data.success) {
+                navigate('/')
+                setToken(data.token)
+                localStorage.setItem('token', data.token)
+                setShowLogin(false)
+            }else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)       
+        }
+    
     }
 
   return (
     <div  onClick={()=>setShowLogin(false)} className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center text-sm text-gray-600 bg-black/50'>
+        
       <form  onSubmit={onSubmitHandler} onClick={(e)=>e.stopPropagation()}className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] text-gray-500 rounded-lg shadow-xl border border-gray-200 bg-white">
             <p className="text-2xl font-medium m-auto">
                 <span className="text-primary">User</span> {state === "login" ? "Login" : "Sign Up"}
@@ -33,14 +53,14 @@ const Login = ({setShowLogin}) => {
             </div>
             {state === "register" ? (
                 <p>
-                    Already have account? <span onClick={() => setState("login")} className="text-indigo-500 cursor-pointer">click here</span>
+                    Already have account? <span onClick={() => setState("login")} className="text-primary cursor-pointer">click here</span>
                 </p>
             ) : (
                 <p>
-                    Create an account? <span onClick={() => setState("register")} className="text-indigo-500 cursor-pointer">click here</span>
+                    Create an account? <span onClick={() => setState("register")} className="text-primary cursor-pointer">click here</span>
                 </p>
             )}
-            <button className="bg-primary hover:bg-blue -600 transition-all text-white w-full py-2 rounded-md cursor-pointer">
+            <button className="bg-primary hover:bg-blue -800 transition-all text-white w-full py-2 rounded-md cursor-pointer">
                 {state === "register" ? "Create Account" : "Login"}
             </button>
         </form>
